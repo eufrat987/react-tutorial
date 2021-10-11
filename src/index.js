92bc8c41984a774a3cb2926936dde60e62727698
luk <lukzyw@gmail.com>
2021-10-11 14:27:41 +0000

    added book component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book 
+      title = 'I Love You to the Moon and Back' 
+      author = 'Amelia Hepworth' 
+      img = "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"  
+      />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Book 
+      title = 'American Marxism' 
+      author = 'Mark R. Levin'
+      img = "https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL200_SR200,200_.jpg"  
+      />
+ 
+    </section>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+
+  // const title = 'I Love You to the Moon and Back'
+  // const author = 'Amelia Hepworth'
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1> { title } </h1>
+      <h4> { author } </h4>
+      
+    </article>
+  )
+}
+
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
