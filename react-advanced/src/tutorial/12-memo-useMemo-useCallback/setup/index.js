f85fd697d6506d4fb6602516f69e35060206e63c
Łukasz Żywański <lukasz.zywanski@sollers.eu>
2021-10-24 18:14:41 +0200

    end of tutorial

--- react-advanced/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ react-advanced/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -4,12 +4,31 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
-
+  const expensiveValue = (data) => {
+    console.log(data)
+    return( 
+      data.reduce( (total, item) => {
+        if(item.fields.price > total) {
+          total = item.fields.price
+        }
+        return total
+      }, 0) / 100
+    )
+  }
 // every time props or state changes, component re-renders
 
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [card, setCard] = useState(0)
+
+  const addToCard = useCallback(() => {
+    setCard(card => {
+      return card + 1
+    })
+  }, [])
+
+  const value = useMemo(() => expensiveValue(products), [products])
 
   return (
     <>
@@ -17,22 +36,28 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1>Card: {card}</h1>
+      <h4>Expensice : {value} </h4>
+      <BigList products={products} addToCard={addToCard}/>
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCard }) => {
+  useEffect(() => {
+    console.log('list rerender')
+  })
+
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCard={addToCard}></SingleProduct>
       })}
     </section>
   )
-}
+})
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCard }) => {
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +67,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCard}>add</button>
     </article>
   )
 }
